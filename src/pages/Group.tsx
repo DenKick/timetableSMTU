1f68dc27dd83973da8a55cd544a0a718703a1a4c
Denis Brester <49287397+DenKick@users.noreply.github.com>
2021-10-30 23:08:51 +0300

    feat (screen): Add Group Screen

---
+++ src/pages/Group.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import GroupHeader from '../components/GroupHeader/GroupHeader';
+import {useRoute} from '@react-navigation/core';
+import {ScrollView} from 'react-native';
+
+import Accordeon from '../components/Accordeon/Accordeon';
+import SubjectCard from '../components/SubjectCard/SubjectCard';
+
+import {days} from '../configs/days';
+
+import {GroupScreenRoutePropList} from '../types/types';
+
+const RenderDays = () => {
+  const renderedDays = days.map(item => {
+    return (
+      <Accordeon key={item} title={item}>
+        <SubjectCard
+          title="Высшая математика"
+          timeStart="10:10"
+          timeEnd="11:40"
+          teacher="Сорокин Вадим Николаевич"
+          type="Лекция"
+          audience="ЦДО Дистанционно"
+        />
+        <SubjectCard
+          title="Химия"
+          timeStart="11:50"
+          timeEnd="13:20"
+          teacher="Богданова Светлана Ефимовна"
+          type="Лекция"
+          audience="ЦДО Дистанционно"
+        />
+      </Accordeon>
+    );
+  });
+
+  return <>{renderedDays}</>;
+};
+
+const Group: React.FC = () => {
+  const route = useRoute<GroupScreenRoutePropList>();
+  const groupId = route.params.id;
+
+  return (
+    <>
+      <GroupHeader title={groupId} />
+      <ScrollView>
+        <RenderDays />
+      </ScrollView>
+    </>
+  );
+};
+
+export default Group;
