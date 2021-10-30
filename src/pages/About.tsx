9e40171977f5776942cefdcee3e19f06fa4a5bc5
Denis Brester <49287397+DenKick@users.noreply.github.com>
2021-10-30 23:09:07 +0300

    feat (screen): Add About Screen

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../components/Header/Header';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Header heading="О приложении" />
+    </>
+  );
+};
+
+export default Home;
