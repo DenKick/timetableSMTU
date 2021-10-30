c1eea97c549b1b6be4bbede7dbe4958f091366ad
Denis Brester <49287397+DenKick@users.noreply.github.com>
2021-10-30 23:08:21 +0300

    feat (utils): Add Types file

---
+++ src/types/types.ts
@@ -0,0 +1,67 @@
+import {ReactNode} from 'react';
+import {RouteProp} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+  Home: undefined;
+  Group: {id: string};
+  About: undefined;
+};
+
+type HomeScreenParams = NativeStackNavigationProp<RootStackParamList, 'Home'>;
+type GroupScreenParams = NativeStackNavigationProp<RootStackParamList, 'Group'>;
+type AboutScreenParams = NativeStackNavigationProp<RootStackParamList, 'About'>;
+
+type GroupScreenRouteProp = RouteProp<RootStackParamList, 'Group'>;
+
+type HeaderProps = {
+  heading: string;
+  withSearch?: boolean;
+  withImage?: boolean;
+};
+
+type AboutHeaderProps = {
+  heading: string;
+};
+
+type GroupLinkProps = {
+  title: string;
+};
+
+type SubjectCardProps = {
+  title: string;
+  timeStart: string;
+  timeEnd: string;
+  teacher: string;
+  audience: string;
+  type: string;
+};
+
+type GroupHeaderProps = {
+  title: string;
+};
+
+type AccordeonProps = {
+  title: string;
+  children: ReactNode | null;
+};
+
+type SearchBarProps = {
+  withClearButton: boolean;
+  onInputText?: Function;
+};
+
+export type RootStackParamTypes = RootStackParamList;
+
+export type HomeScreenParamTypes = HomeScreenParams;
+export type GroupScreenParamTypes = GroupScreenParams;
+export type AboutScreenParamTypes = AboutScreenParams;
+export type GroupScreenRoutePropList = GroupScreenRouteProp;
+
+export type HeaderPropList = HeaderProps;
+export type AboutHeaderPropList = AboutHeaderProps;
+export type GroupLinkPropList = GroupLinkProps;
+export type SubjectCardPropList = SubjectCardProps;
+export type GroupHeaderPropList = GroupHeaderProps;
+export type AccordeonPropList = AccordeonProps;
+export type SearchBarPropList = SearchBarProps;
