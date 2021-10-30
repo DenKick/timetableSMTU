119e742d0fd907e7b1be0ed49acc1e7211f0fb5d
Denis Brester <49287397+DenKick@users.noreply.github.com>
2021-10-30 23:11:56 +0300

    feat (App): Add navigation to the App

--- App.tsx
+++ App.tsx
@@ -1,115 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  createNativeStackNavigator,
+  NativeStackNavigationOptions,
+} from '@react-navigation/native-stack';
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+import Home from './src/pages/Home';
+import Group from './src/pages/Group';
+import About from './src/pages/About';
+
+import {DarkTheme, LightTheme} from './src/themes/Themes';
+
+type RootStackParamList = {
+  Home: undefined;
+  Group: {id: string};
+  About: undefined;
 };
 
+const RootStack = createNativeStackNavigator<RootStackParamList>();
+
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const scheme = useColorScheme() === 'dark';
+
+  const options: NativeStackNavigationOptions = {
+    headerShown: false,
+  };
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    height: '100%',
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer theme={scheme ? DarkTheme : LightTheme}>
+      <StatusBar barStyle={scheme ? 'light-content' : 'dark-content'} />
+      <SafeAreaView style={backgroundStyle}>
+        <RootStack.Navigator initialRouteName={'Home'}>
+          <RootStack.Screen name="Home" component={Home} options={options} />
+          <RootStack.Screen name="Group" component={Group} options={options} />
+          <RootStack.Screen name="About" component={About} options={options} />
+        </RootStack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
