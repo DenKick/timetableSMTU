41fce33a3908d7fe5c2131253cb4c2a3c92349d8
Denis Brester <49287397+DenKick@users.noreply.github.com>
2021-10-30 23:07:03 +0300

    feat (utils): Add themes

---
+++ src/themes/Themes.ts
@@ -0,0 +1,25 @@
+const DarkTheme = {
+  dark: true,
+  colors: {
+    primary: 'rgb(10, 132, 255)',
+    background: 'rgb(0, 0, 0)',
+    text: 'rgb(255, 255, 255)',
+    border: 'rgb(44, 44, 46)',
+    card: 'rgb(28, 28, 30)',
+    notification: 'rgb(142, 142, 147)',
+  },
+};
+
+const LightTheme = {
+  dark: false,
+  colors: {
+    primary: 'rgb(0, 122, 255)',
+    background: 'rgb(255, 255, 255)',
+    card: 'rgb(242, 242, 247)',
+    text: 'rgb(0, 0, 0)',
+    border: 'rgb(199, 199, 204)',
+    notification: 'rgb(142, 142, 147)',
+  },
+};
+
+export {DarkTheme, LightTheme};
